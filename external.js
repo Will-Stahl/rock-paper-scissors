7fdedef737ba74e2f805919ee03a0357698fb12f
Will-Stahl <billdstahl@gmail.com>
2022-06-07 11:21:47 -0500

    UI feature completed in rsi-ui branch

--- external.js
+++ external.js
@@ -1,4 +1,9 @@
 /* external.js */
+
+let rounds = 0;
+let playerWins = 0;
+let computerWins = 0;
+
 function computerPlay() {
     let res = Math.floor(Math.random()*3);
     if (res === 0) {
@@ -12,47 +17,85 @@ function computerPlay() {
     }
 }
 
+function addLine(message) {
+    const body = document.querySelector("#main");
+    const paragraph = document.createElement("par");
+    const line_break = document.createElement("br");
+    paragraph.textContent = message;
+    body.appendChild(paragraph);
+    body.appendChild(line_break);
+}
+
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
+    rounds++;
     if (playerSelection === "rock" && computerSelection === "scissors") {
-        console.log("You're winner! Rock beats scissors!");
-        return 1;
+        addLine("You're winner! Rock beats scissors!");
+        playerWins++;
     }
     else if (playerSelection === "scissors" && computerSelection === "paper") {
-        console.log("You're winner! Scissors beats paper!");
-        return 1;
+        addLine("You're winner! Scissors beats paper!");
+        playerWins++;
     }
     else if (playerSelection === "paper" && computerSelection === "rock") {
-        console.log("You're winner! Paper beats rock!");
-        return 1;
+        addLine("You're winner! Paper beats rock!");
+        playerWins++;
     }
     else {
-        console.log("You did lose!");
-        return 0;
+        addLine("You did lose!");
+        computerWins++;
+    }
+    addLine("Round " + rounds.toString() + ". " + "Player: " +
+            playerWins.toString() + ", Computer: " + computerWins.toString());
+    if (rounds === 5) {
+        if (playerWins > 2) {
+            addLine("Game complete. You win!");
+        }
+        else {
+            addLine("Game complete. You had big lose...");
+        }
     }
 }
 
 function game() {
-    let playerWins = 0;
-    let computerWins = 0;
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Enter \"rock, paper, or scissors\".", null);
-        // playRound() is case insensitive
-        result = playRound(playerSelection, computerPlay());
-        if (result) {
-            playerWins++;
-        } else {
-            computerWins++;
-        }
-    }
-    console.log("Game complete.\nPlayer wins: " + playerWins.toString())
-    console.log("Computer wins: " + computerWins.toString())
-    if (playerWins > computerWins) {
-        console.log("You won all!");
-    } else {
-        console.log("You had big lose...");
-    }
+    // let playerWins = 0;
+    // let computerWins = 0;
+    // for (let i = 0; i < 5; i++) {
+    //     let playerSelection = prompt("Enter \"rock, paper, or scissors\".", null);
+    //     // playRound() is case insensitive
+    //     result = playRound(playerSelection, computerPlay());
+    //     if (result) {
+    //         playerWins++;
+    //     } else {
+    //         computerWins++;
+    //     }
+    // }
+    // console.log("Game complete.\nPlayer wins: " + playerWins.toString())
+    // console.log("Computer wins: " + computerWins.toString())
+    // if (playerWins > computerWins) {
+    //     console.log("You won all!");
+    // } else {
+    //     console.log("You had big lose...");
+    // }
+    let playerSelection = prompt("Enter \"rock, paper, or scissors\".", null);
+    playRound(playerSelection, computerPlay());
 }
 
-game();
+// game();
+
+// event listeners
+const rock_btn = document.querySelector('#rock');
+rock_btn.addEventListener('click', () => {
+    playRound("rock", computerPlay());
+});
+
+const paper_btn = document.querySelector('#paper');
+paper_btn.addEventListener('click', () => {
+    playRound("paper", computerPlay());
+});
+
+const scissors_btn = document.querySelector('#scissors');
+scissors_btn.addEventListener('click', () => {
+    playRound("scissors", computerPlay());
+});
